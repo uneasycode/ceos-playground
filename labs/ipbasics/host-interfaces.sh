57fa3c89482ebcde231f1b999c140e29524c9895
uneasycode <remixdevil4u@gmail.com>
2025-07-10 11:34:41 -0400

    IPBasics Lab with configs and Topology

--- labs/ipbasics/host-interfaces.sh
+++ labs/ipbasics/host-interfaces.sh
@@ -1,34 +1,11 @@
 
-
 #!/bin/sh
-sudo docker exec host1 ip link set eth0 up
-sudo docker exec host1 ip addr add 192.168.101.3/24 dev eth1
 
 
-sudo docker exec host1 ip link set eth1 up
-sudo docker exec host1 ip addr add 103.0.0.1/24 dev eth1
-sudo docker exec host1 ip route add 192.168.0.0/16 via 103.0.0.254 dev eth1
-sudo docker exec host1 ip route add 10.10.10.0/24 via 103.0.0.254 dev eth1
-
-
-
-sudo docker exec host2 ip link set eth0 up
-sudo docker exec host2 ip addr add 192.168.101.4/24 dev eth1
-
-
-sudo docker exec host1 ip link set eth1 up
 sudo docker exec host1 ip addr add 103.0.0.2/24 dev eth1
-sudo docker exec host1 ip route add 192.168.0.0/16 via 103.0.0.254 dev eth1
-sudo docker exec host1 ip route add 10.10.10.0/24 via 103.0.0.254 dev eth1
+sudo docker exec host2 ip addr add 103.0.0.3/24 dev eth1
+sudo docker exec server ip addr add 103.0.0.3/24 dev eth2
+sudo docker exec host2 ip addr add 104.0.0.3/24 dev eth1
+sudo docker exec host1ip route add 104.0.0.0/24 via 103.0.0.254 dev eth1
 
-
-
-sudo docker exec server ip link set eth0 up
-sudo docker exec server ip addr add 192.168.101.4/24 dev eth1
-
-
-sudo docker exec server ip link set eth1 up
-sudo docker exec server ip addr add 103.0.0.2/24 dev eth1
-sudo docker exec server ip route add 192.168.0.0/16 via 103.0.0.254 dev eth1
-sudo docker exec server ip route add 10.10.10.0/24 via 103.0.0.254 dev eth1
 
